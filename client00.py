98fa5f060ac7de568a280519b21ec2fed02692a7
root <root@localhost.localdomain>
2017-05-16 23:52:21 +0900

    高画質のURLを出力するようにした。

--- client00.py
+++ client00.py
@@ -10,5 +10,7 @@ content = requests.get(URL).content.decode('UTF-8')
 p = re.compile('http://lss.line-cdn.net/p/live.+?chunklist.m3u8')
 matchResult = p.findall(content)
 
-print(matchResult[-1])
+
+# 高画質のURLに変換して、出力をする
+print(re.sub('/360/', '/720/', matchResult[-1]))
 

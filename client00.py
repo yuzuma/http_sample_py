14256bd022bf84becf0004e895b463e531c38714
yuzuma <yuzuma.yuzuma.yuzuma@gmail.com>
2017-06-04 20:04:23 +0900

    update

--- client00.py
+++ client00.py
@@ -6,13 +6,13 @@ import re
 from time import sleep
 
 # weather news
-# URL = 'https://live.line.me/channels/659/broadcast/3055754'
+#URL = 'https://live.line.me/channels/659/broadcast/3055754'
 
 # ann0
 # URL = 'https://live.line.me/channels/231/broadcast/295248'
 
 # test
-URL = 'https://live.line.me/channels/34989/broadcast/3103498'
+URL = 'https://live.line.me/channels/231/upcoming/3183899'
 
 while True:
     content = requests.get(URL).content.decode('UTF-8')
@@ -20,11 +20,10 @@ while True:
     p = re.compile('http://lss.line-cdn.net/p/live.+?chunklist.m3u8')
 
     matchResult = p.findall(content)
-    print(len(matchResult))
-    if len(matchResult) > 1:
+    if len(matchResult) > 0:
         break
     else :
-        sleep(33)
+        sleep(30)
 
 
 # 高画質のURLに変換して、出力をする
